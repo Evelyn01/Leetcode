6fb13000307c603dfe83fa112dcafde8e2d4d0af
Bo <titanduan1083@gmail.com>
2014-05-19 09:48:08 -0700

    Add single number solution.

---
+++ src/com/leetcode/singlenumber/Solution.java
@@ -0,0 +1,17 @@
+package com.leetcode.singlenumber;
+
+public class Solution {
+	public int singleNumber(int[] A) {
+
+		if (A == null || A.length < 1) {
+			return 0;
+		}
+
+		int single = A[0];
+		for (int i = 1; i < A.length; i++) {
+			single ^= A[i];
+
+		}
+		return single;
+	}
+}
