6d93c75c228c1e89fd372b7efa32760e585a7085
duanbo1983 <duanbo1983@gmail.com>
2014-08-07 16:24:34 -0700

    Improve the tester.

--- src/com/leetcode/sorting/SortTester.java
+++ src/com/leetcode/sorting/SortTester.java
@@ -4,6 +4,30 @@ package com.leetcode.sorting;
  * Created by bod on 8/2/14.
  */
 public class SortTester {
+    private static int[][] cases = {
+            {},
+            {0},
+            {0, 0},
+            {0, 0, 0},
+            {0, 1},
+            {1, 0},
+            {0, 1, 2},
+            {0, 2, 1},
+            {1, 0, 2},
+            {1, 2, 0},
+            {2, 0, 1},
+            {2, 1, 0},
+            {0, 1, 1},
+            {1, 0, 1},
+            {1, 1, 0},
+            {3, 5, 1, 9, 8, 7, 4},
+            {1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+            {10, 9, 8, 7, 6, 5, 4, 3, 2, 1},
+            {42, 9, 17, 54, 602, -3, 54, 999, -11},
+            {-11, -3, 9, 17, 42, 54, 54, 602, 999},
+    };
+
+
     Sorter sorter;
 
     public SortTester(Sorter sorter) {
@@ -11,26 +35,10 @@ public class SortTester {
     }
 
     public void testSort() {
-        int[] testArray = new int[]{3, 5, 1, 9, 8, 7, 4};
-        sorter.sort(testArray);
-
-        printArray(testArray);
-
-        testArray = new int[]{};
-        sorter.sort(null);
-        printArray(testArray);
-
-        testArray = new int[]{};
-        sorter.sort(testArray);
-        printArray(testArray);
-
-        testArray = new int[]{1};
-        sorter.sort(testArray);
-        printArray(testArray);
-
-        testArray = new int[]{1, 2};
-        sorter.sort(testArray);
-        printArray(testArray);
+        for (int[] testArray : cases) {
+            sorter.sort(testArray);
+            printArray(testArray);
+        }
     }
 
     private void printArray(int[] array) {
