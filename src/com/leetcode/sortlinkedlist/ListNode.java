896c7657b75e46ca6f8e35faf7df6acde9b98a74
duanbo1983 <duanbo1983@gmail.com>
2014-08-09 15:08:50 -0700

    Add sort list impl.

---
+++ src/com/leetcode/sortlinkedlist/ListNode.java
@@ -0,0 +1,15 @@
+package com.leetcode.sortlinkedlist;
+
+/**
+ * Created by titan-developer on 8/8/14.
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
+
