0a8040435dc0c5316c722672c27f08950d2f57f2
duanbo1983 <duanbo1983@gmail.com>
2014-08-22 22:30:46 -0700

    Add blank solution for word break.

---
+++ src/com/leetcode/wordbreak/WordBreakSolution.java
@@ -0,0 +1,19 @@
+package com.leetcode.wordbreak;
+
+import java.util.Set;
+
+/**
+ * Created by bod on 8/19/14.
+ */
+public class WordBreakSolution {
+
+
+    public boolean wordBreak(String s, Set<String> dict) {
+        if (s == null || s.length() <= 1) {
+            return false;
+        }
+
+
+        return false;
+    }
+}
