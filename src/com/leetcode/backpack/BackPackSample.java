0e8f2449e64efb2de49b1e337d47b2c79f62685c
duanbo1983 <duanbo1983@gmail.com>
2014-08-23 13:20:33 -0700

    Add backpack blank solution.

---
+++ src/com/leetcode/backpack/BackPackSample.java
@@ -0,0 +1,24 @@
+package com.leetcode.backpack;
+
+/**
+ * Created by bod on 8/23/14.
+ */
+public class BackPackSample {
+    static int[][] diamonds = new int[][] {
+            {2, 10},
+            {3, 7},
+            {4, 1},
+            {5, 4},
+            {8, 2},
+            {6, 8},
+            {1, 3},
+            {5, 6},
+            {9, 8},
+    };
+
+    public int[] backPackSolution(int[][] diamonds, int packSize) {
+
+
+
+    }
+}
