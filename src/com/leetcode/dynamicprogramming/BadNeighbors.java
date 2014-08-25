4d76017be73943d82a5c59b5faad79324da38861
duanbo1983 <duanbo1983@gmail.com>
2014-08-24 19:09:17 -0700

    Finish zigzag solution.

---
+++ src/com/leetcode/dynamicprogramming/BadNeighbors.java
@@ -0,0 +1,10 @@
+package com.leetcode.dynamicprogramming;
+
+/**
+ * Created by bod on 8/24/14.
+ * Problem description: http://community.topcoder.com/stat?c=problem_statement&pm=2402&rd=5009
+ */
+public class BadNeighbors {
+
+
+}
--- src/com/leetcode/dynamicprogramming/ZigZag.java
+++ src/com/leetcode/dynamicprogramming/ZigZag.java
@@ -2,7 +2,7 @@ package com.leetcode.dynamicprogramming;
 
 /**
  * Created by bod on 8/24/14.
- * http://community.topcoder.com/tc?module=ProblemDetail&rd=4493&pm=1259
+ * Description: http://community.topcoder.com/tc?module=ProblemDetail&rd=4493&pm=1259
  */
 public class ZigZag {
 
