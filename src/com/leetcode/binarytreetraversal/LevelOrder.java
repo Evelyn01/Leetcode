4f3623a0d03837719de08d2be8822878da7b1c76
duanbo1983 <duanbo1983@gmail.com>
2014-08-31 00:40:14 -0700

    Add zigzag level traversal.

--- src/com/leetcode/binarytreetraversal/LevelOrder.java
+++ src/com/leetcode/binarytreetraversal/LevelOrder.java
@@ -20,14 +20,99 @@ public class LevelOrder {
         root.right.right = new TreeNode(7);
 
         LevelOrder order = new LevelOrder();
-        List<List<Integer>> list = order.levelOrderBottom(root);
+        outputList(order.levelOrder(root));
+        outputList(order.levelOrderBottom(root));
+        outputList(order.levelOrderZigZag(root));
+    }
 
+    public static void outputList(List<List<Integer>> list) {
         for (List<Integer> intList : list) {
             for (int a : intList) {
                 System.out.print(a + " , ");
             }
             System.out.println("");
         }
+        System.out.println("--------------");
+    }
+
+    public List<List<Integer>> levelOrderZigZag(TreeNode treeNode) {
+        ArrayList<List<Integer>> order = new ArrayList<List<Integer>>();
+        if (treeNode == null) {
+            return order;
+        }
+
+        ArrayList<TreeNode> list = new ArrayList<TreeNode>();
+        list.add(treeNode);
+
+        int lastLevelNumber = 1;
+        int currentLevelNumber = 0;
+        boolean isRightOrder = false;
+        while (list.size() > 0) {
+            ArrayList<Integer> currentLevel = new ArrayList<Integer>();
+            currentLevelNumber = 0;
+            for (int i = 0 ; i < lastLevelNumber ; i ++) {
+                TreeNode tempNode = list.get(0);
+
+                if (isRightOrder)
+                    currentLevel.add(0, tempNode.val);
+                else
+                    currentLevel.add(tempNode.val);
+
+                if (tempNode.left != null) {
+                    currentLevelNumber ++;
+                    list.add(tempNode.left);
+                }
+
+                if (tempNode.right != null) {
+                    currentLevelNumber ++;
+                    list.add(tempNode.right);
+                }
+                list.remove(0);
+            }
+            lastLevelNumber = currentLevelNumber;
+            order.add(currentLevel);
+            isRightOrder = !isRightOrder;
+        }
+
+
+        return order;
+    }
+
+    public List<List<Integer>> levelOrder(TreeNode treeNode) {
+        ArrayList<List<Integer>> order = new ArrayList<List<Integer>>();
+        if (treeNode == null) {
+            return order;
+        }
+
+        ArrayList<TreeNode> list = new ArrayList<TreeNode>();
+        list.add(treeNode);
+
+        int lastLevelNumber = 1;
+        int currentLevelNumber = 0;
+        while (list.size() > 0) {
+            ArrayList<Integer> currentLevel = new ArrayList<Integer>();
+            currentLevelNumber = 0;
+            for (int i = 0 ; i < lastLevelNumber ; i ++) {
+                TreeNode tempNode = list.get(0);
+                currentLevel.add(tempNode.val);
+
+                if (tempNode.left != null) {
+                    currentLevelNumber ++;
+                    list.add(tempNode.left);
+                }
+
+                if (tempNode.right != null) {
+                    currentLevelNumber ++;
+                    list.add(tempNode.right);
+                }
+                list.remove(0);
+            }
+            lastLevelNumber = currentLevelNumber;
+            order.add(currentLevel);
+        }
+
+
+        return order;
     }
 
     public List<List<Integer>> levelOrderBottom(TreeNode treeNode) {
