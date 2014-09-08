576aba484eff96935558172748db40e45d90166f
duanbo1983 <duanbo1983@gmail.com>
2014-09-07 21:39:01 -0700

    Fix an error.

--- src/com/leetcode/symmetrictree/SymmetricTree.java
+++ src/com/leetcode/symmetrictree/SymmetricTree.java
@@ -2,72 +2,19 @@ package com.leetcode.symmetrictree;
 
 import com.leetcode.common.TreeNode;
 
-import java.util.ArrayList;
-
 /**
- * Created by titan-developer on 9/6/14.
+ * Created by bod on 9/7/14.
  */
 public class SymmetricTree {
-    ArrayList<String> nodeArr;
-
-    public static void main(String[] strings) {
-//        TreeNode root = new TreeNode(1);
-//        root.left = new TreeNode(2);
-//        root.left.left = new TreeNode(4);
-//        root.left.right = new TreeNode(3);
-//
-//        root.right = new TreeNode(2);
-//        root.right.left = new TreeNode(3);
-//        root.right.right = new TreeNode(4);
-
-        TreeNode root = new TreeNode(5);
-        root.left = new TreeNode(4);
-        root.left.right = new TreeNode(1);
-        root.left.right.left = new TreeNode(2);
-
-        root.right = new TreeNode(1);
-        root.right.right = new TreeNode(4);
-        root.right.right.left = new TreeNode(2);
-
-        SymmetricTree tree = new SymmetricTree();
-
-        System.out.println(tree.isSymmetric(root));
-    }
-
     public boolean isSymmetric(TreeNode root) {
-        // Start typing your Java solution below
-        // DO NOT write main() function
-        if (root == null) {
-            return true;
-        }
-        nodeArr = new ArrayList<String>();
-        inorderTraversal(root);
-
-        // compare in-order traversal list
-        int i = 0;
-        int j = nodeArr.size() - 1;
-        while (i < j) {
-            if (!nodeArr.get(i).equals(nodeArr.get(j))) {
-                return false;
-            }
-            i++;
-            j--;
-        }
-        return true;
+        if(root == null) return true;
+        else return areSymSubtrees(root.left, root.right);
     }
-
-    public void inorderTraversal(TreeNode n) {
-        // if empty node, save as ‘#’ to occupy this place
-        if (n == null) {
-            nodeArr.add("#");
-            return;
-        }
-        if (n.left == null && n.right == null) {
-            nodeArr.add("" + n.val);
-        } else {
-            inorderTraversal(n.left);
-            nodeArr.add("" + n.val);
-            inorderTraversal(n.right);
-        }
+    public boolean areSymSubtrees(TreeNode node1, TreeNode node2){
+        if(node1 == null || node2==null) return ((node1 == null) && (node2 == null));
+        if(node1.val != node2.val) return false;
+        boolean outsideSym = areSymSubtrees(node1.left, node2.right);
+        boolean insideSym = areSymSubtrees(node1.right, node2.left);
+        return outsideSym && insideSym;
     }
 }
---
+++ src/com/leetcode/symmetrictree/SymmetricTreeWrongSolution.java
@@ -0,0 +1,73 @@
+package com.leetcode.symmetrictree;
+
+import com.leetcode.common.TreeNode;
+
+import java.util.ArrayList;
+
+/**
+ * Created by titan-developer on 9/6/14.
+ */
+public class SymmetricTreeWrongSolution {
+    ArrayList<String> nodeArr;
+
+    public static void main(String[] strings) {
+//        TreeNode root = new TreeNode(1);
+//        root.left = new TreeNode(2);
+//        root.left.left = new TreeNode(4);
+//        root.left.right = new TreeNode(3);
+//
+//        root.right = new TreeNode(2);
+//        root.right.left = new TreeNode(3);
+//        root.right.right = new TreeNode(4);
+
+        TreeNode root = new TreeNode(5);
+        root.left = new TreeNode(4);
+        root.left.right = new TreeNode(1);
+        root.left.right.left = new TreeNode(2);
+
+        root.right = new TreeNode(1);
+        root.right.right = new TreeNode(4);
+        root.right.right.left = new TreeNode(2);
+
+        SymmetricTreeWrongSolution tree = new SymmetricTreeWrongSolution();
+
+        System.out.println(tree.isSymmetric(root));
+    }
+
+    public boolean isSymmetric(TreeNode root) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        if (root == null) {
+            return true;
+        }
+        nodeArr = new ArrayList<String>();
+        inorderTraversal(root);
+
+        // compare in-order traversal list
+        int i = 0;
+        int j = nodeArr.size() - 1;
+        while (i < j) {
+            if (!nodeArr.get(i).equals(nodeArr.get(j))) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+    public void inorderTraversal(TreeNode n) {
+        // if empty node, save as ‘#’ to occupy this place
+        if (n == null) {
+            nodeArr.add("#");
+            return;
+        }
+        if (n.left == null && n.right == null) {
+            nodeArr.add("" + n.val);
+        } else {
+            inorderTraversal(n.left);
+            nodeArr.add("" + n.val);
+            inorderTraversal(n.right);
+        }
+    }
+}
