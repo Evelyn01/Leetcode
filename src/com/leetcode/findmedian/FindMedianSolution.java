8a3d0c24d5946991ea01a5e93e993d7d92e076ea
duanbo1983 <duanbo1983@gmail.com>
2014-09-09 22:57:10 -0700

    Add find median.

---
+++ src/com/leetcode/findmedian/FindMedianSolution.java
@@ -0,0 +1,31 @@
+package com.leetcode.findmedian;
+
+import java.util.ArrayList;
+
+/**
+ * Created by bod on 9/9/14.
+ * https://oj.leetcode.com/problems/median-of-two-sorted-arrays/
+ */
+public class FindMedianSolution {
+
+
+    public double findMedianSortedArrays(int A[], int B[]) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        int m = A.length - 1;
+        int n = B.length - 1;
+        while (m >= 0 && n >= 0) {
+            if (A[m] > B[n]) {
+                list.add(A[m--]);
+            } else {
+                list.add(B[n--]);
+            }
+        }
+        while (m >= 0) {
+            list.add(A[m--]);
+        }
+        while (n >= 0) {
+            list.add(B[n--]);
+        }
+        return (list.get(list.size() >> 1) + list.get((list.size() - 1) >> 1)) / 2.0;
+    }
+}
