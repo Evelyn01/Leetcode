90a59b3724d6c26c9f3f16144d5127be9381ef4d
duanbo1983 <duanbo1983@gmail.com>
2014-09-09 23:28:38 -0700

    Add blank solution for longestPalindromic.

---
+++ src/com/leetcode/longestpalindromic/LongestPalindromic.java
@@ -0,0 +1,12 @@
+package com.leetcode.longestpalindromic;
+
+/**
+ * Created by bod on 9/9/14.
+ * https://oj.leetcode.com/problems/longest-palindromic-substring/
+ */
+public class LongestPalindromic {
+
+    public String longestPalindrome(String s) {
+        return null;
+    }
+}
