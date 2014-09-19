d629c589bdce5d9e13ba4c3966676c7b41bb42f0
duanbo1983 <duanbo1983@gmail.com>
2014-09-18 19:57:18 -0700

    Add regular expression.

---
+++ src/com/leetcode/regularexpression/RegularExpression.java
@@ -0,0 +1,20 @@
+package com.leetcode.regularexpression;
+
+/**
+ * Created by titan-developer on 9/17/14.
+ * https://oj.leetcode.com/problems/regular-expression-matching/
+ */
+public class RegularExpression {
+
+     public boolean isMatch(String s, String p) {
+         if (s == null || p == null || s.trim().length() == 0 || p.trim().length() == 0) {
+             return false;
+         }
+
+
+
+
+
+         return false;
+     }
+}
