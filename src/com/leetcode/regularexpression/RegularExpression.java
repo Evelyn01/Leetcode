9981af2b2e0222a120d47dd0c40a89070ac0a73a
duanbo1983 <duanbo1983@gmail.com>
2014-09-18 21:09:57 -0700

    Add regular expression.

--- src/com/leetcode/regularexpression/RegularExpression.java
+++ src/com/leetcode/regularexpression/RegularExpression.java
@@ -6,15 +6,94 @@ package com.leetcode.regularexpression;
  */
 public class RegularExpression {
 
-     public boolean isMatch(String s, String p) {
-         if (s == null || p == null || s.trim().length() == 0 || p.trim().length() == 0) {
-             return false;
-         }
 
+    /*
+    Some examples:
+    isMatch("aa","a") → false
+    isMatch("aa","aa") → true
+    isMatch("aaa","aa") → false
+    isMatch("aa", "a*") → true
+    isMatch("aa", ".*") → true
+    isMatch("ab", ".*") → true
+    isMatch("aab", "c*a*b") → true
+    */
 
+    public static void main(String[] strings) {
+        RegularExpression regularExpression = new RegularExpression();
+        System.out.println(regularExpression.isMatch("aaa", "ab*a*c*a"));
+        System.out.println(regularExpression.isMatch("aaa", "a*a"));
+        System.out.println(regularExpression.isMatch("aa", "a"));
+        System.out.println(regularExpression.isMatch("aa", "aa"));
+        System.out.println(regularExpression.isMatch("aaa", "aa"));
+        System.out.println(regularExpression.isMatch("aa", "a*"));
+        System.out.println(regularExpression.isMatch("aa", ".*"));
+        System.out.println(regularExpression.isMatch("ab", ".*"));
+        System.out.println(regularExpression.isMatch("aab", "c*a*b"));
 
+    }
 
+    public boolean isMatch(String s, String p) {
+        if (s == null || p == null || s.trim().length() == 0 || p.trim().length() == 0) {
+            return false;
+        }
 
-         return false;
-     }
+        int index_p = 0;
+        int index_s = 0;
+
+        int spareCount = 0;
+        String spareStr = "";
+
+        while (index_p < p.length() && index_s < s.length()) {
+            String p_str = p.substring(index_p, index_p + 1);
+            index_p++;
+            boolean isWild = false;
+
+            if (index_p < p.length()) {
+                String wild = p.substring(index_p, index_p + 1);
+                if (wild.equals("*")) {
+                    isWild = true;
+                    index_p++;
+
+                    while (index_p < p.length()) {
+                        String temp = p.substring(index_p, index_p + 1);
+                        if (temp.equals(p_str) || temp.equals("*")) {
+                            index_p ++;
+                        } else {
+                            break;
+                        }
+                    }
+                }
+            }
+
+            String ss = s.substring(index_s, index_s + 1);
+            if (p_str.equals(".") || p_str.equals(ss)) {
+                if (isWild) {
+                    index_s++;
+                    if (index_s >= s.length())
+                        break;
+
+                    ss = s.substring(index_s, index_s + 1);
+                    while (p_str.equals(".") || p_str.equals(ss)) {
+                        index_s++;
+                        if (index_s >= s.length()) {
+                            break;
+                        }
+                        ss = s.substring(index_s, index_s + 1);
+                    }
+                } else {
+                    index_s++;
+                }
+            } else if (isWild) {
+                //index_p ++;
+            } else {
+                return false;
+            }
+        }
+
+        if (index_p == p.length() && index_s == s.length()) {
+            return true;
+        }
+
+        return false;
+    }
 }
