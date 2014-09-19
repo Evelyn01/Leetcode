bda367c318f0adb98e85151aec579b96631e8350
duanbo1983 <duanbo1983@gmail.com>
2014-09-18 22:55:17 -0700

    Finally got it.

--- src/com/leetcode/regularexpression/RegularExpression.java
+++ src/com/leetcode/regularexpression/RegularExpression.java
@@ -20,6 +20,19 @@ public class RegularExpression {
 
     public static void main(String[] strings) {
         RegularExpression regularExpression = new RegularExpression();
+
+        System.out.println(regularExpression.isMatch("bbbba", ".*a*a"));
+        System.out.println(regularExpression.isMatch("ab", ".*c"));
+        System.out.println(regularExpression.isMatch("aaa", "ab*a*c*a"));
+        System.out.println(regularExpression.isMatch("aaa", "a*a"));
+        System.out.println(regularExpression.isMatch("aa", "a"));
+        System.out.println(regularExpression.isMatch("aa", "aa"));
+        System.out.println(regularExpression.isMatch("aaa", "aa"));
+        System.out.println(regularExpression.isMatch("aa", "a*"));
+        System.out.println(regularExpression.isMatch("aa", ".*"));
+        System.out.println(regularExpression.isMatch("ab", ".*"));
+        System.out.println(regularExpression.isMatch("aab", "c*a*b"));
+
         System.out.println(regularExpression.isMatchWrong("bbbba", ".*a*a"));
         System.out.println(regularExpression.isMatchWrong("ab", ".*c"));
         System.out.println(regularExpression.isMatchWrong("aaa", "ab*a*c*a"));
@@ -33,8 +46,49 @@ public class RegularExpression {
         System.out.println(regularExpression.isMatchWrong("aab", "c*a*b"));
     }
 
+    boolean matchFirst(String s, String p) {
+        if (s.length() == 0 && p.length() == 0) {
+            return true;
+        }
+
+        if (s.length() > 0 && p.length() > 0) {
+            return s.substring(0, 1).equals(p.substring(0, 1)) || p.substring(0, 1).equals(".");
+        }
+
+        return false;
+    }
 
     public boolean isMatch(String s, String p) {
+        if (s == null || p == null) {
+            return false;
+        }
+
+        if (s.length() == 0 && p.length() == 0) {
+            return true;
+        }
+
+        if (p.length() > 1 && p.substring(1, 2).equals("*")) {
+            if (isMatch(s, p.substring(2))) {
+                return true;
+            } else {
+                while (matchFirst(s, p)) {
+                    s = s.substring(1);
+                    if (isMatch(s, p.substring(2))) {
+                        return true;
+                    }
+                }
+            }
+        } else {
+            if (s.length() == 0 || p.length() == 0) {
+                return false;
+            }
+
+            if (matchFirst(s, p)) {
+                return isMatch(s.substring(1), p.substring(1));
+            } else {
+                return false;
+            }
+        }
 
         return false;
     }
@@ -67,8 +121,8 @@ public class RegularExpression {
                 if (isWild) {
                     continue;
                 } else {
-                    if (spareStr.equals(p_str) && spareCount > 0)  {
-                        spareCount --;
+                    if (spareStr.equals(p_str) && spareCount > 0) {
+                        spareCount--;
                     } else {
                         return false;
                     }
@@ -88,7 +142,7 @@ public class RegularExpression {
                             index_s++;
 
                             if (ss.equals(spareStr)) {
-                                spareCount ++;
+                                spareCount++;
                             } else {
                                 spareStr = ss;
                                 spareCount = 1;
