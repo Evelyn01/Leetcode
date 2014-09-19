454a03803953f7df56b164b4dee0716e5f0a5bc5
duanbo1983 <duanbo1983@gmail.com>
2014-09-18 21:39:47 -0700

    Fix a few issues but still fail.

--- src/com/leetcode/regularexpression/RegularExpression.java
+++ src/com/leetcode/regularexpression/RegularExpression.java
@@ -20,6 +20,8 @@ public class RegularExpression {
 
     public static void main(String[] strings) {
         RegularExpression regularExpression = new RegularExpression();
+        System.out.println(regularExpression.isMatch("bbbba", ".*a*a"));
+        System.out.println(regularExpression.isMatch("ab", ".*c"));
         System.out.println(regularExpression.isMatch("aaa", "ab*a*c*a"));
         System.out.println(regularExpression.isMatch("aaa", "a*a"));
         System.out.println(regularExpression.isMatch("aa", "a"));
@@ -43,7 +45,7 @@ public class RegularExpression {
         int spareCount = 0;
         String spareStr = "";
 
-        while (index_p < p.length() && index_s < s.length()) {
+        while (index_p < p.length()) {
             String p_str = p.substring(index_p, index_p + 1);
             index_p++;
             boolean isWild = false;
@@ -54,39 +56,63 @@ public class RegularExpression {
                     isWild = true;
                     index_p++;
 
-                    while (index_p < p.length()) {
-                        String temp = p.substring(index_p, index_p + 1);
-                        if (temp.equals(p_str) || temp.equals("*")) {
-                            index_p ++;
-                        } else {
-                            break;
-                        }
-                    }
+//                    while (index_p < p.length()) {
+//                        String temp = p.substring(index_p, index_p + 1);
+//                        if (temp.equals(p_str) || temp.equals("*")) {
+//                            index_p ++;
+//                        } else {
+//                            break;
+//                        }
+//                    }
                 }
             }
 
-            String ss = s.substring(index_s, index_s + 1);
-            if (p_str.equals(".") || p_str.equals(ss)) {
+            if (index_s == s.length()) {
                 if (isWild) {
-                    index_s++;
-                    if (index_s >= s.length())
-                        break;
-
-                    ss = s.substring(index_s, index_s + 1);
-                    while (p_str.equals(".") || p_str.equals(ss)) {
-                        index_s++;
-                        if (index_s >= s.length()) {
-                            break;
-                        }
-                        ss = s.substring(index_s, index_s + 1);
-                    }
+                    continue;
                 } else {
-                    index_s++;
+                    if (spareStr.equals(p_str) && spareCount > 0)  {
+                        spareCount --;
+                    } else {
+                        return false;
+                    }
                 }
-            } else if (isWild) {
-                //index_p ++;
             } else {
-                return false;
+                String ss = s.substring(index_s, index_s + 1);
+                if (p_str.equals(".") || p_str.equals(ss)) {
+                    if (isWild) {
+                        spareStr = ss;
+                        spareCount++;
+                        index_s++;
+                        if (index_s >= s.length())
+                            break;
+
+                        ss = s.substring(index_s, index_s + 1);
+                        while (p_str.equals(".") || p_str.equals(ss)) {
+                            index_s++;
+
+                            if (ss.equals(spareStr)) {
+                                spareCount ++;
+                            } else {
+                                spareStr = ss;
+                                spareCount = 1;
+                            }
+
+                            if (index_s >= s.length()) {
+                                break;
+                            }
+                            ss = s.substring(index_s, index_s + 1);
+                        }
+                    } else {
+                        spareCount = 0;
+                        spareStr = "";
+                        index_s++;
+                    }
+                } else if (isWild) {
+                    //index_p ++;
+                } else {
+                    return false;
+                }
             }
         }
 
