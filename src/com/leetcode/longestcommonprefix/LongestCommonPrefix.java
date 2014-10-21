d2aa0cf45fb22a8ec441d35189ba601ba0831f36
duanbo1983 <duanbo1983@gmail.com>
2014-10-20 19:02:49 -0700

    Impl remon to integer. Add blank longest common prefix.

---
+++ src/com/leetcode/longestcommonprefix/LongestCommonPrefix.java
@@ -0,0 +1,12 @@
+package com.leetcode.longestcommonprefix;
+
+/**
+ * Created by titan-developer on 10/20/14.
+ */
+public class LongestCommonPrefix {
+
+    public String longestCommonPrefix(String[] strs) {
+
+        return null;
+    }
+}
