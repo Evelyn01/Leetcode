2b45eb05b0995859a0d4cbc18a34f7420c7d2185
duanbo1983 <duanbo1983@gmail.com>
2014-10-20 21:35:29 -0700

    Just format code.

--- src/com/leetcode/sum3/Sum3.java
+++ src/com/leetcode/sum3/Sum3.java
@@ -24,25 +24,25 @@ public class Sum3 {
     public List<List<Integer>> threeSum(int[] num) {
         List resultList = new ArrayList();
         Arrays.sort(num);
-        for(int i = 0; i < num.length; i ++){
-            if(i > 0 && num[i] == num[i-1])
+        for (int i = 0; i < num.length; i++) {
+            if (i > 0 && num[i] == num[i - 1])
                 continue;
-            if(num[i] > 0){
+            if (num[i] > 0) {
                 break;
-            }else{
-                for(int j = i+1, k = num.length - 1; k > j;){
-                    if(j > i+1 && num[j] == num[j-1]){
+            } else {
+                for (int j = i + 1, k = num.length - 1; k > j; ) {
+                    if (j > i + 1 && num[j] == num[j - 1]) {
                         j++;
                         continue;
                     }
-                    if(num[j]+ num[i] > 0){
+                    if (num[j] + num[i] > 0) {
                         break;
                     }
-                    if(num[j]+ num[i] + num[k] < 0)
+                    if (num[j] + num[i] + num[k] < 0)
                         j++;
-                    else if(num[j]+ num[i] + num[k] > 0)
+                    else if (num[j] + num[i] + num[k] > 0)
                         k--;
-                    else{
+                    else {
                         List ll = new ArrayList();
                         ll.add(num[i]);
                         ll.add(num[j]);
