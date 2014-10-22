3d59c11b2ff8316f487fe2d84991a0f1c4fbd2b6
duanbo1983 <duanbo1983@gmail.com>
2014-10-22 11:30:27 -0700

    Partial solution.

--- src/com/leetcode/maximumproduct/MaximumProductSubarray.java
+++ src/com/leetcode/maximumproduct/MaximumProductSubarray.java
@@ -13,11 +13,66 @@ public class MaximumProductSubarray {
         System.out.println(maxProduct.maxProduct(a));
     }
 
+//    public int maxProduct2(int[] array) {
+//        int maxProduct = Integer.MIN_VALUE;
+//
+//        if (array == null || array.length < 2) {
+//            return maxProduct;
+//        }
+//
+//        int minus = 0;
+//
+//        int current = 0;
+//
+//        for (int i = 0 ; i < array.length ; i ++) {
+//            if (array[i])
+//        }
+//
+//        return maxProduct;
+//    }
+
     public int maxProduct(int[] array) {
         int maxProduct = Integer.MIN_VALUE;
 
         if (array == null || array.length < 2) {
             return maxProduct;
+        }
+
+        int minus = 0;
+        maxProduct = array[0] * array[1];
+        int maxLastIndex = 1;
+        int currentMax = 0;
+
+        for (int i = 2 ; i < array.length ; i ++) {
+            if (maxProduct < 0) {
+                minus = maxProduct;
+                if (array[i] <= 0) {
+                    maxProduct = maxProduct * array[i];
+                    maxLastIndex = i;
+                } else {
+                    if (array[i - 1] > 0) {
+                        maxProduct = array[i - 1] * array[i];
+                        maxLastIndex = i;
+                    } else {
+                        minus = minus * array[i];
+                    }
+                }
+            } else {
+                if (maxLastIndex == i - 1) {
+                    if (array[i] > 0) {
+                        maxProduct = maxProduct * array[i];
+                        maxLastIndex = i;
+                    } else if (array[i] == 0) {
+                        minus = 0;
+                    } else {
+                        minus = minus * array[i];
+                    }
+                } else {
+
+                }
+
+
+            }
         }
 
         return maxProduct;
