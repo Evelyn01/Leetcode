c0b3f47c8c171cc213d011dcb690108925da70a2
duanbo1983 <duanbo1983@gmail.com>
2014-10-22 21:17:20 -0700

    Update format.

--- src/com/leetcode/maximumproduct/MaximumProductSubarray.java
+++ src/com/leetcode/maximumproduct/MaximumProductSubarray.java
@@ -29,34 +29,34 @@ public class MaximumProductSubarray {
         // Initialize overall max product
         int max_so_far = 1;
 
-    /* Traverse throught the array. Following values are maintained after the ith iteration:
-       max_ending_here is always 1 or some positive product ending with arr[i]
-       min_ending_here is always 1 or some negative product ending with arr[i] */
+        /* Traverse throught the array. Following values are maintained after the ith iteration:
+           max_ending_here is always 1 or some positive product ending with arr[i]
+           min_ending_here is always 1 or some negative product ending with arr[i] */
         for (int i = 0; i < arr.length; i++)
         {
-        /* If this element is positive, update max_ending_here. Update
-           min_ending_here only if min_ending_here is negative */
+            /* If this element is positive, update max_ending_here. Update
+               min_ending_here only if min_ending_here is negative */
             if (arr[i] > 0)
             {
                 max_ending_here = max_ending_here*arr[i];
                 min_ending_here = Math.min (min_ending_here * arr[i], 1);
             }
 
-        /* If this element is 0, then the maximum product cannot
-           end here, make both max_ending_here and min_ending_here 0
-           Assumption: Output is alway greater than or equal to 1. */
+            /* If this element is 0, then the maximum product cannot
+               end here, make both max_ending_here and min_ending_here 0
+               Assumption: Output is alway greater than or equal to 1. */
             else if (arr[i] == 0)
             {
                 max_ending_here = 1;
                 min_ending_here = 1;
             }
 
-        /* If element is negative. This is tricky
-           max_ending_here can either be 1 or positive. min_ending_here can either be 1
-           or negative.
-           next min_ending_here will always be prev. max_ending_here * arr[i]
-           next max_ending_here will be 1 if prev min_ending_here is 1, otherwise
-           next max_ending_here will be prev min_ending_here * arr[i] */
+            /* If element is negative. This is tricky
+               max_ending_here can either be 1 or positive. min_ending_here can either be 1
+               or negative.
+               next min_ending_here will always be prev. max_ending_here * arr[i]
+               next max_ending_here will be 1 if prev min_ending_here is 1, otherwise
+               next max_ending_here will be prev min_ending_here * arr[i] */
             else
             {
                 int temp = max_ending_here;
