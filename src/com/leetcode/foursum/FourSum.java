9be714056e6dcffbda7beff989308f0f1f520d67
duanbo1983 <duanbo1983@gmail.com>
2014-10-23 15:24:00 -0700

    Add good solution for 4Sum.

--- src/com/leetcode/foursum/FourSum.java
+++ src/com/leetcode/foursum/FourSum.java
@@ -5,7 +5,7 @@ import java.util.*;
 /**
  * Created by titan-developer on 10/23/14.
  * https://oj.leetcode.com/problems/4sum/
- *
+ * <p/>
  * Solution:
  * http://www.cnblogs.com/tenosdoit/p/3649607.html
  */
@@ -21,43 +21,55 @@ public class FourSum {
     public List<List<Integer>> fourSum(int[] num, int target) {
         int n = num.length;
         List<List<Integer>> res = new ArrayList<List<Integer>>();
-        HashMap<Integer, Vector<int[]>> pairs = new HashMap<Integer, Vector<int[]>>(n * n);
+        HashMap<Integer, List<int[]>> pairs = new HashMap<Integer, List<int[]>>(n * n);
         Arrays.sort(num);
 
-        for(int i = 0; i < n; i++)
-            for(int j = i+1 ; j < n; j++) {
-                if (pairs.containsKey(num[i]+num[j])) {
-                    Vector<int[]> list = pairs.get(num[i]+num[j]);
+        //-2, -1, 0, 0, 1, 2
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (pairs.containsKey(num[i] + num[j])) {
+                    List<int[]> list = pairs.get(num[i] + num[j]);
                     list.add(new int[]{i, j});
                 } else {
-                    Vector<int[]> list = new Vector<int[]>();
+                    ArrayList<int[]> list = new ArrayList<int[]>();
                     list.add(new int[]{i, j});
-                    pairs.put(num[i]+num[j], list);
+                    pairs.put(num[i] + num[j], list);
                 }
             }
+        }
 
-        for(int i = 0; i < n - 3; i++)
-        {
-            if(i != 0 && num[i] == num[i-1])continue;//防止第一个元素重复
-            for(int j = i+1; j < n - 2; j++)
-            {
-                if(j != i+1 && num[j] == num[j-1])continue;//防止第二个元素重复
-                if(pairs.containsKey(target - num[i] - num[j]))
-                {
-                    Vector<int[]> sum2 = pairs.get(target - num[i] - num[j]);
+        for (int i = 0; i < n - 3; i++) {
+
+            //avoid first element's duplication.
+            if (i != 0 && num[i] == num[i - 1]) {
+                continue;
+            }
+
+            for (int j = i + 1; j < n - 2; j++) {
+
+                //avoid second element's duplication.
+                if (j != i + 1 && num[j] == num[j - 1]) {
+                    continue;
+                }
+
+                if (pairs.containsKey(target - num[i] - num[j])) {
+                    List<int[]> sum2 = pairs.get(target - num[i] - num[j]);
                     boolean isFirstPush = true;
-                    for(int k = 0; k < sum2.size(); k++)
-                    {
+                    for (int k = 0; k < sum2.size(); k++) {
                         int[] indexes = sum2.get(k);
-                        if(indexes[0] <= j)continue;//保证所求的四元组的数组下标是递增的
-                        if(isFirstPush || res.get(res.size() - 1).get(2) != num[indexes[0]])
-                        {
-                            Vector<Integer> list = new Vector<Integer>();
+
+                        //ensure the index is non-decrease.
+                        if (indexes[0] <= j) {
+                            continue;
+                        }
+                        if (isFirstPush || res.get(res.size() - 1).get(2) != num[indexes[0]]) {
+                            ArrayList<Integer> list = new ArrayList<Integer>();
                             list.add(num[i]);
                             list.add(num[j]);
                             list.add(num[indexes[0]]);
                             list.add(num[indexes[1]]);
                             isFirstPush = false;
+                            res.add(list);
                         }
                     }
                 }
