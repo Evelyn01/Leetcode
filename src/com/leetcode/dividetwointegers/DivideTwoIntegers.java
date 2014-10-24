92fe23ee71733d6d4232d8dbcd5f226f21bee58b
duanbo1983 <duanbo1983@gmail.com>
2014-10-24 10:10:28 -0700

    Better solution now.

--- src/com/leetcode/dividetwointegers/DivideTwoIntegers.java
+++ src/com/leetcode/dividetwointegers/DivideTwoIntegers.java
@@ -9,7 +9,11 @@ public class DivideTwoIntegers {
     public static void main(String[] strings) {
 
         DivideTwoIntegers divider = new DivideTwoIntegers();
-        System.out.println(divider.divide(50, 16));
+//        System.out.println(divider.divide(33, -3));
+//        System.out.println(divider.divide(30, 16));
+//        System.out.println(divider.divide(100, 2));
+//        System.out.println(divider.divide(1, -1));
+        System.out.println(divider.divide(-1010369383, -2147483648));
     }
 
     public int divide(int dividend, int divisor) {
@@ -17,8 +21,33 @@ public class DivideTwoIntegers {
             throw new RuntimeException("divide 0");
         }
 
-        if (dividend < divisor) {
+        boolean isPositive = true;
+
+        if (dividend < 0 && divisor < 0) {
+            dividend = - dividend;
+            divisor = - divisor;
+        } else if (dividend < 0 && divisor > 0) {
+            dividend = - dividend;
+            isPositive = false;
+        } else if (dividend > 0 && divisor < 0) {
+            divisor = - divisor;
+            isPositive = false;
+        }
+
+        if (dividend == 0 || dividend < divisor) {
             return 0;
+        }
+
+        if (dividend == divisor) {
+            return isPositive ? 1 : -1;
+        }
+
+        if (divisor == 1) {
+            return isPositive ? dividend : - dividend;
+        }
+
+        if (divisor == 2) {
+            return isPositive? ( dividend >> 1 ) : - ( dividend >> 1 );
         }
 
         int ori = dividend;
@@ -26,9 +55,14 @@ public class DivideTwoIntegers {
         while (dividend > divisor) {
             dividend = dividend >> 1;
             bit ++;
+            if (dividend == divisor) {
+                return isPositive ? ( 1 << bit ) : - ( 1 << bit );
+            }
         }
         bit --;
 
-        return 1 << bit + divide(ori - dividend << 1, divisor);
+        int a = ( 1 << bit ) ;
+        int b = divide(ori - ( divisor << bit ), divisor);
+        return isPositive ? a + b : - (a + b);
     }
 }
