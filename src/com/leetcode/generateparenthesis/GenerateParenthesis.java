e091d94713bf254866bc09a9e1e1e558b00b9334
duanbo1983 <duanbo1983@gmail.com>
2014-10-23 18:16:54 -0700

    Good solution for generate parenthesis.

--- src/com/leetcode/generateparenthesis/GenerateParenthesis.java
+++ src/com/leetcode/generateparenthesis/GenerateParenthesis.java
@@ -12,21 +12,56 @@ public class GenerateParenthesis {
     public static void main(String[] strings) {
 
         GenerateParenthesis parenthesisList = new GenerateParenthesis();
-        List<String> list = parenthesisList.generateParenthesis(3);
-        for (String parenthesis : list) {
-            System.out.println(parenthesis);
+        List<String> list = parenthesisList.generateParenthesis(4);
+        for (int i = 0 ; i < list.size() ; i ++) {
+            System.out.println(i + " : " + list.get(i));
         }
     }
 
     public List<String> generateParenthesis(int n) {
-        List<String> list = new ArrayList<String>();
-
         if (n <= 0) {
-            return list;
+            return null;
         }
 
-
+        List<String> list = recursiveGenerateParenthesis(n);
 
         return list;
+    }
+
+    private List<String> recursiveGenerateParenthesis(int n) {
+        List<String> arrayList = new ArrayList<String>();
+
+        if (n == 0) {
+            return arrayList;
+        } else if (n == 1) {
+            String one = "()";
+            arrayList.add(one);
+            return arrayList;
+        } else {
+            for (int i = 0 ; i < n ; i ++) {
+                List<String> insideArray = recursiveGenerateParenthesis(i);
+                List<String> outsideArray = recursiveGenerateParenthesis(n - i - 1);
+
+                if (insideArray.size() == 0) {
+                    for (int k = 0 ; k < outsideArray.size() ; k ++) {
+                        String temp = "()" + outsideArray.get(k);
+                        arrayList.add(temp);
+                    }
+                } else if (outsideArray.size() == 0) {
+                    for (int m = 0 ; m < insideArray.size() ; m ++) {
+                        String temp = "(" + insideArray.get(m) + ")";
+                        arrayList.add(temp);
+                    }
+                } else {
+                    for (int m = 0 ; m < insideArray.size() ; m ++) {
+                        String temp = "(" + insideArray.get(m) + ")";
+                        for (int k = 0 ; k < outsideArray.size() ; k ++) {
+                            arrayList.add(temp + outsideArray.get(k));
+                        }
+                    }
+                }
+            }
+        }
+        return arrayList;
     }
 }
