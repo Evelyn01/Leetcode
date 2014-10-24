0effece22925a8ca7f248e9d5fee9e507a1ef6ef
duanbo1983 <duanbo1983@gmail.com>
2014-10-24 12:43:24 -0700

    Add blank solution for remove element.

---
+++ src/com/leetcode/removeelement/RemoveElement.java
@@ -0,0 +1,11 @@
+package com.leetcode.removeelement;
+
+/**
+ * Created by titan-developer on 10/24/14.
+ */
+public class RemoveElement {
+
+    public int removeElement(int[] A, int elem) {
+
+    }
+}
