e6b5baee41c0975d6307066888e0ec10db16cb52
duanbo1983 <duanbo1983@gmail.com>
2014-10-24 12:05:33 -0700

    Add blank solution for valid parentheses.

---
+++ src/com/leetcode/validparentheses/ValidParentheses.java
@@ -0,0 +1,20 @@
+package com.leetcode.validparentheses;
+
+/**
+ * Created by titan-developer on 10/24/14.
+ * https://oj.leetcode.com/problems/valid-parentheses/
+ */
+public class ValidParentheses {
+
+    public static void main(String[] strings) {
+
+        String a = "()[]{}";
+
+        ValidParentheses parentheses = new ValidParentheses();
+        System.out.println(parentheses.isValid(a));
+    }
+
+    public boolean isValid(String s) {
+        return false;
+    }
+}
