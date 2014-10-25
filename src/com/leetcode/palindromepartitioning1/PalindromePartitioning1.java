f24668960c7049e8cca0c5a42763bcb463a01e2e
duanbo1983 <duanbo1983@gmail.com>
2014-10-25 15:20:58 -0700

    Add a good solution.

--- src/com/leetcode/palindromepartitioning1/PalindromePartitioning1.java
+++ src/com/leetcode/palindromepartitioning1/PalindromePartitioning1.java
@@ -1,7 +1,5 @@
 package com.leetcode.palindromepartitioning1;
 
-import com.sun.org.apache.xerces.internal.xs.StringList;
-
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
@@ -26,6 +24,41 @@ public class PalindromePartitioning1 {
     }
 
     public List<List<String>> partition(String s) {
+        int len = s.length();
+        List<List<String>>[] result = new List[len + 1];
+        result[0] = new ArrayList<List<String>>();
+        result[0].add(new ArrayList<String>());
+
+        boolean[][] pair = new boolean[len][len];
+        for(int i=0; i<s.length(); i++){
+            result[i+1] = new ArrayList<List<String>>();
+            char c = s.charAt(i);
+            for(int j=0; j<=i; j++){
+                if(j == i)
+                    pair[j][i] = true;
+                else{
+                    if(s.charAt(j) != c)
+                        continue;
+                    if(j == i-1)
+                        pair[j][i] = true;
+                    else
+                        pair[j][i] = pair[j+1][i-1];
+                }
+                if(pair[j][i]){
+                    String str = s.substring(j, i+1);
+                    for(List<String> r : result[j]){
+                        List<String> ri = new ArrayList<String>(r);
+                        ri.add(str);
+                        result[i+1].add(ri);
+                    }
+                }
+            }
+        }
+
+        return result[len];
+    }
+
+    public List<List<String>> partition_2(String s) {
 
         List<List<String>> result = new LinkedList<List<String>>();
 
