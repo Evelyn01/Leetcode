5927f33e90e8dd8af59f67ddc0dbfdd3552dd5fe
duanbo1983 <duanbo1983@gmail.com>
2014-10-25 22:05:29 -0700

    Add accepted solution.

--- src/com/leetcode/palindromepartitioning2/PalindromePartitioning2.java
+++ src/com/leetcode/palindromepartitioning2/PalindromePartitioning2.java
@@ -10,8 +10,8 @@ public class PalindromePartitioning2 {
 
     public static void main(String[] strings) {
 
-        String s = "ababababababababababababcbabababababababababababa";
-        //String s = "x";
+        //String s = "ababababababababababababcbabababababababababababa";
+        String s = "xaabaac";
 
         PalindromePartitioning2 partitioning2 = new PalindromePartitioning2();
 
@@ -19,44 +19,24 @@ public class PalindromePartitioning2 {
     }
 
     public int minCut(String s) {
-        int len = s.length();
-        List<List<String>>[] result = new List[len + 1];
-        result[0] = new ArrayList<List<String>>();
-        result[0].add(new ArrayList<String>());
-        int minForResultN = Integer.MAX_VALUE;
+        if (s == null || s.length() == 0)
+            return 0;
 
-        boolean[][] pair = new boolean[len][len];
-        for (int i = 0; i < s.length(); i++) {
-            result[i + 1] = new ArrayList<List<String>>();
-            char c = s.charAt(i);
-            for (int j = 0; j <= i; j++) {
-                if (j == i)
-                    pair[j][i] = true;
-                else {
-                    if (s.charAt(j) != c)
-                        continue;
-                    if (j == i - 1)
-                        pair[j][i] = true;
-                    else
-                        pair[j][i] = pair[j + 1][i - 1];
-                }
-                if (pair[j][i]) {
-                    String str = s.substring(j, i + 1);
-                    for (List<String> r : result[j]) {
-                        List<String> ri = new ArrayList<String>(r);
-                        ri.add(str);
-                        result[i + 1].add(ri);
-
-                        if (i + 1 == len) {
-                            if (ri.size() < minForResultN) {
-                                minForResultN = ri.size();
-                            }
-                        }
-                    }
+        int n = s.length();
+        boolean m[][] = new boolean[n][n];
+        int d[] = new int[n];
+        for (int i = n - 1; i >= 0; i--) {
+            d[i] = n - i - 1;
+            for (int j = i; j < n; j++) {
+                if (s.charAt(i) == s.charAt(j) && (j - i < 2 || m[i + 1][j - 1])) {
+                    m[i][j] = true;
+                    if (j == n - 1)
+                        d[i] = 0;
+                    else if (d[j + 1] + 1 < d[i])
+                        d[i] = d[j + 1] + 1;
                 }
             }
         }
-
-        return minForResultN;
+        return d[0];
     }
 }
