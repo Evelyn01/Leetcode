8df6b1bf2ac3bfaba85265770279ac5418fbbeac
duanbo1983 <duanbo1983@gmail.com>
2014-10-29 23:05:34 -0700

    Formatting.

--- src/com/leetcode/regularexpression/RegularExpression.java
+++ src/com/leetcode/regularexpression/RegularExpression.java
@@ -71,23 +71,23 @@ public class RegularExpression {
         int m = s.length();
         int n = p.length();
 
-        boolean[][] OPT = new boolean[m+1][n+1];
+        boolean[][] OPT = new boolean[m + 1][n + 1];
         OPT[0][0] = true;
 
         for (int i = 1; i <= m; i++) {
             OPT[i][0] = false;
         }
         for (int j = 1; j <= n; j++) {
-            OPT[0][j] = (p.charAt(j-1) == '*') && (j-2 >= 0) && OPT[0][j-2];
+            OPT[0][j] = (p.charAt(j - 1) == '*') && (j - 2 >= 0) && OPT[0][j - 2];
         }
 
         for (int i = 1; i <= m; i++) {
             for (int j = 1; j <= n; j++) {
-                OPT[i][j] = ((OPT[i-1][j-1]) && equals(s, p, i-1, j-1))
-                        ||  ((OPT[i-1][j] || OPT[i][j-1])
-                        && (p.charAt(j-1) == '*')
-                        && equals(s, p, i-1, j-2))
-                        ||  ((p.charAt(j-1) == '*') && (j-2 >= 0) && OPT[i][j-2]);
+                OPT[i][j] = ((OPT[i - 1][j - 1]) && equals(s, p, i - 1, j - 1))
+                        || ((OPT[i - 1][j] || OPT[i][j - 1])
+                        && (p.charAt(j - 1) == '*')
+                        && equals(s, p, i - 1, j - 2))
+                        || ((p.charAt(j - 1) == '*') && (j - 2 >= 0) && OPT[i][j - 2]);
             }
         }
 
