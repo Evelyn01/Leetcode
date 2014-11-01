31b7ab394e7621cc326d57b6e5b0f9ea2e0b128f
duanbo1983 <duanbo1983@gmail.com>
2014-10-31 22:39:53 -0700

    Add an accepted solution.

--- src/com/leetcode/jumpgame2/JumpGame2.java
+++ src/com/leetcode/jumpgame2/JumpGame2.java
@@ -7,8 +7,8 @@ public class JumpGame2 {
 
     public static void main(String[] strings) {
 
-        //int[] a = {2, 3, 1, 1, 4};
-        int[] a = {2, 1};
+        int[] a = {2, 3, 1, 1, 4};
+        //int[] a = {2, 1};
 
         JumpGame2 jumper = new JumpGame2();
         System.out.println(jumper.jump(a));
@@ -23,16 +23,45 @@ public class JumpGame2 {
             return 0;
         }
 
+        int range[] = new int[2];
+        range[0] = 0;
+        range[1] = 0;
+        int step = 0;
+
+        while (range[1] < a.length - 1) {
+            int max = 0;
+            for (int i = range[0] ; i <= range[1] ; i ++) {
+                if (i + a[i] > max) {
+                    max = i + a[i];
+                }
+            }
+            range[0] = range[1] + 1;
+            range[1] = max;
+            step ++;
+        }
+
+        return step;
+    }
+
+    public int jumpDP(int[] a) {
+        if (a == null || a.length == 0) {
+            return -1;
+        }
+
+        if (a.length == 1) {
+            return 0;
+        }
+
         int step[] = new int[a.length];
 
         step[0] = 0;
 
-        for (int x = 1 ; x < a.length ; x ++) {
+        for (int x = 1; x < a.length; x++) {
             step[x] = Integer.MAX_VALUE;
         }
 
-        for (int i = 0 ; i < a.length - 1 ; i ++) {
-            for (int j = 1 ; j <= a[i] ; j ++) {
+        for (int i = 0; i < a.length - 1; i++) {
+            for (int j = 1; j <= a[i]; j++) {
                 if (i + j < a.length && step[i] + 1 < step[i + j]) {
                     step[i + j] = step[i] + 1;
                 }
