c4938a1d8c7ff3ae11fa342aa7fce2687172e47c
duanbo1983 <duanbo1983@gmail.com>
2014-10-31 18:22:08 -0700

    Add blank solution.

---
+++ src/com/leetcode/recovertree/RecoverBinarySTree.java
@@ -0,0 +1,21 @@
+package com.leetcode.recovertree;
+
+import com.leetcode.common.TreeNode;
+
+/**
+ * Created by titan-developer on 10/31/14.
+ */
+public class RecoverBinarySTree {
+
+    public static void main(String[] strings) {
+
+    }
+
+    public void recoverTree(TreeNode root) {
+
+    }
+
+    private void swapNodes(TreeNode a, TreeNode a_parent, TreeNode b, TreeNode b_parent) {
+
+    }
+}
