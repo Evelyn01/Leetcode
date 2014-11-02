7c22a40be6a92da05e4b75062a0ee93b80ba2a37
duanbo1983 <duanbo1983@gmail.com>
2014-11-02 00:30:03 -0700

    Add an accepted solution.

--- src/com/leetcode/permutationsequence/PermutationSequence.java
+++ src/com/leetcode/permutationsequence/PermutationSequence.java
@@ -11,19 +11,25 @@ public class PermutationSequence {
         System.out.println(permutationSequence.getPermutation(4, 10));
     }
 
-    public String getPermutation(int n, int k) {
-
-        int size = 1;
-
-        for (int i = n ; i > 1 ; i --) {
-            size *= i;
+    String getPermutation(int n, int k) {
+        int i, j, f = 1;
+        // left part of s is partially formed permutation, right part is the leftover chars.
+        char[] chars = new char[n];
+        for (i = 1; i <= n; i++) {
+            f *= i;
+            chars[i - 1] = (char)('0' + i); // make s become 1234...n
         }
-
-        if (k > size) {
-            return "";
+        for (i = 0, k--; i < n; i++) {
+            f /= n - i;
+            j = i + k / f; // calculate index of char to put at s[i]
+            char c = chars[j];
+            // remove c by shifting to cover up (adjust the right part).
+            for (; j > i; j--)
+                chars[j] = chars[j - 1];
+            k %= f;
+            chars[i] = c;
         }
-
-        return "";
+        return new String(chars);
     }
 
 }
