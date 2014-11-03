de51ee51b11570e48f87b71c37358fd66ddff22a
duanbo1983 <duanbo1983@gmail.com>
2014-11-02 19:17:47 -0800

    Add an accepted solution. And some utility.

--- src/com/leetcode/common/ListNode.java
+++ src/com/leetcode/common/ListNode.java
@@ -11,5 +11,44 @@ public class ListNode {
         val = x;
         next = null;
     }
+
+    //"1->1->2->3->3"
+    public static ListNode createList(String state) {
+        if (state == null || state.trim().length() == 0) {
+            return new ListNode(-1);
+        }
+
+        int index = state.indexOf("->");
+        int cursor = 0;
+        ListNode head = null, last = null;
+        while (index != -1) {
+            String valStr = state.substring(cursor, index);
+            ListNode temp = new ListNode(Integer.valueOf(valStr));
+            if (head == null) {
+                head = temp;
+                last = temp;
+            } else {
+                last.next = temp;
+                last = temp;
+            }
+            cursor = index + 2;
+            index = state.indexOf("->", cursor);
+        }
+
+        if (last != null && cursor < state.length()) {
+            String valStr = state.substring(cursor);
+            last.next = new ListNode(Integer.valueOf(valStr));
+        }
+
+        return head;
+    }
+
+    public static void print(ListNode head) {
+        while (head != null) {
+            System.out.print(head.val + ", ");
+            head = head.next;
+        }
+        System.out.println();
+    }
 }
 
