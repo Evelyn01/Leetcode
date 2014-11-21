32b5b3ff022576c560b083eeac1a27a8b336c197
duanbo1983 <duanbo1983@gmail.com>
2014-11-20 20:33:46 -0800

    Preorder.

--- src/com/practise1/PreorderTravsal.java
+++ src/com/practise1/PreorderTravsal.java
@@ -1,8 +1,11 @@
 package com.practise1;
 
 import com.leetcode.common.TreeNode;
+import com.leetcode.common.TreeNodeCreator;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 /**
  * Created by bod on 11/20/14.
@@ -10,11 +13,34 @@ import java.util.List;
 public class PreorderTravsal {
 
     public static void main(String[] strings) {
+        TreeNode root = TreeNodeCreator.createTreeNode("1, 2, 3, 4, 5, 6, 7, #, #, #, #, 8");
 
+        PreorderTravsal preorderTravsal = new PreorderTravsal();
+
+        List<Integer> list = preorderTravsal.preorderTraversal(root);
+
+        for (int v : list) {
+            System.out.printf(v + ", ");
+        }
     }
 
     public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<Integer>();
 
-        return null;
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+
+        while(root != null) {
+            list.add(root.val);
+            if (root.right != null) {
+                stack.push(root);
+            }
+
+            root = root.left;
+            if (root == null && !stack.isEmpty()) {
+                root = stack.pop().right;
+            }
+        }
+
+        return list;
     }
 }
