4b6fc57b23e4911b4214991ec0e06fbf38bc5203
duanbo1983 <duanbo1983@gmail.com>
2014-11-20 18:02:13 -0800

    Add treeNode creator.

---
+++ src/com/practise1/PreorderTravsal.java
@@ -0,0 +1,20 @@
+package com.practise1;
+
+import com.leetcode.common.TreeNode;
+
+import java.util.List;
+
+/**
+ * Created by bod on 11/20/14.
+ */
+public class PreorderTravsal {
+
+    public static void main(String[] strings) {
+
+    }
+
+    public List<Integer> preorderTraversal(TreeNode root) {
+
+        return null;
+    }
+}
