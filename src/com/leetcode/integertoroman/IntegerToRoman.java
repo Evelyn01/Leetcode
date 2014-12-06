b733826e1760a8e506ee356dd09a686c3be88966
duanbo1983 <duanbo1983@gmail.com>
2014-12-06 10:56:08 -0800

    Add solution for O(logm + logn) way to find median.

--- src/com/leetcode/integertoroman/IntegerToRoman.java
+++ src/com/leetcode/integertoroman/IntegerToRoman.java
@@ -1,5 +1,7 @@
 package com.leetcode.integertoroman;
 
+import java.util.LinkedHashMap;
+
 /**
  * Created by titan-developer on 10/20/14.
  */
@@ -7,14 +9,43 @@ public class IntegerToRoman {
 
     public static void main(String[] strings) {
         IntegerToRoman integerToRoman = new IntegerToRoman();
-        System.out.println(integerToRoman.intToRoman(10));
-        System.out.println(integerToRoman.intToRoman(2014));
-        System.out.println(integerToRoman.intToRoman(1954));
-        System.out.println(integerToRoman.intToRoman(1990));
-        System.out.println(integerToRoman.intToRoman(1066));
-        System.out.println(integerToRoman.intToRoman(908));
-        System.out.println(integerToRoman.intToRoman(1910));
+        System.out.println(integerToRoman.intToRoman2(10));
+        System.out.println(integerToRoman.intToRoman2(2014));
+        System.out.println(integerToRoman.intToRoman2(1954));
+        System.out.println(integerToRoman.intToRoman2(1990));
+        System.out.println(integerToRoman.intToRoman2(1066));
+        System.out.println(integerToRoman.intToRoman2(908));
+        System.out.println(integerToRoman.intToRoman2(1910));
     }
+
+
+    private static LinkedHashMap<Integer, String> numToRoman = new LinkedHashMap<Integer, String>();
+
+    static {
+        numToRoman.put(1000, "M");
+        numToRoman.put(900, "CM");
+        numToRoman.put(500, "D");
+        numToRoman.put(400, "CD");
+        numToRoman.put(100, "C");
+        numToRoman.put(90, "XC");
+        numToRoman.put(50, "L");
+        numToRoman.put(40, "XL");
+        numToRoman.put(10, "X");
+        numToRoman.put(9, "IX");
+        numToRoman.put(5, "V");
+        numToRoman.put(4, "IV");
+        numToRoman.put(1, "I");
+    }
+
+    public String intToRoman2(int num) {
+        for (Integer i : numToRoman.keySet()) {
+            if (num >= i) {
+                return numToRoman.get(i) + intToRoman(num - i);
+            }
+        }
+        return "";
+    }
+    //--------------------------------------------------
 
     //num is a number from 1 to 3999
     public String intToRoman(int num) {
