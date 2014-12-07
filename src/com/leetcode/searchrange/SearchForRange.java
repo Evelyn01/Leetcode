11f0ca653a14d966f35d74da3fddbf40d9576d92
duanbo1983 <duanbo1983@gmail.com>
2014-12-06 17:55:44 -0800

    Add better solution.

--- src/com/leetcode/searchrange/SearchForRange.java
+++ src/com/leetcode/searchrange/SearchForRange.java
@@ -16,6 +16,58 @@ public class SearchForRange {
         }
     }
 
+    public int[] searchRange2(int[] A, int target) {
+        int[] arr = new int[2];
+        arr[0] = searchLeft(A, target);
+        arr[1] = searchRight(A, target);
+        return arr;
+    }
+
+    private int searchLeft(int[] A, int target) {
+        int s = 0, e = A.length - 1;
+        while (s <= e) {
+            int mid = s + (e - s) / 2;
+            if (A[mid] == target) {
+                if (mid == 0) {
+                    return mid;
+                }
+                if (A[mid - 1] == target) {
+                    e = mid - 1;
+                } else {
+                    return mid;
+                }
+            } else if (target > A[mid]) {
+                s = mid + 1;
+            } else {
+                e = mid - 1;
+            }
+        }
+        return -1;
+    }
+    private int searchRight(int[] A, int target) {
+        int s = 0, e = A.length - 1;
+        while (s <= e) {
+            int mid = s + (e - s) / 2;
+            if (A[mid] == target) {
+                if (mid == A.length - 1) {
+                    return mid;
+                }
+                if (A[mid + 1] == target) {
+                    s = mid + 1;
+                } else {
+                    return mid;
+                }
+            } else if (target > A[mid]) {
+                s = mid + 1;
+            } else {
+                e = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    //----------------------------------------------------------------
+
     public int[] searchRange(int[] a, int target) {
         int[] range = new int[2];
         range[0] = -1;
