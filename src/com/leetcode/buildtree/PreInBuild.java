d3b883eff7c00012195b03b33a402c49cf74af74
duanbo1983 <duanbo1983@gmail.com>
2014-12-08 15:58:34 -0800

    Add better impl.

--- src/com/leetcode/buildtree/PreInBuild.java
+++ src/com/leetcode/buildtree/PreInBuild.java
@@ -17,6 +17,28 @@ public class PreInBuild {
         System.out.println(node.val);
     }
 
+    public TreeNode buildTreeNeat(int[] preorder, int[] inorder) {
+        return helper(0, 0, inorder.length - 1, preorder, inorder);
+    }
+
+    public TreeNode helper(int preStart, int inStart, int inEnd, int[] preorder, int[] inorder) {
+        if (preStart > preorder.length - 1 || inStart > inEnd) {
+            return null;
+        }
+        TreeNode root = new TreeNode(preorder[preStart]);
+        int inIndex = 0; // Index of current root in inorder
+        for (int i = inStart; i <= inEnd; i++) {
+            if (inorder[i] == root.val) {
+                inIndex = i;
+            }
+        }
+        root.left = helper(preStart + 1, inStart, inIndex - 1, preorder, inorder);
+        root.right = helper(preStart + inIndex - inStart + 1, inIndex + 1, inEnd, preorder, inorder);
+        return root;
+    }
+
+    //------------------------------------------------------------------------
+
     public TreeNode buildTree(int[] preOrder, int[] inOrder) {
         if (preOrder == null || preOrder.length == 0 || inOrder == null || inOrder.length == 0) {
             return null;
