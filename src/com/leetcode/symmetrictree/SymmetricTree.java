695cdd92e673ac939731aee61f68562b7fda724e
duanbo1983 <duanbo1983@gmail.com>
2014-12-08 15:03:13 -0800

    clean up the code.

--- src/com/leetcode/symmetrictree/SymmetricTree.java
+++ src/com/leetcode/symmetrictree/SymmetricTree.java
@@ -1,22 +1,27 @@
 package com.leetcode.symmetrictree;
 
 import com.leetcode.common.TreeNode;
+import com.leetcode.common.TreeNodeCreator;
 
 /**
  * Created by bod on 9/7/14.
  * https://oj.leetcode.com/problems/symmetric-tree/
  */
 public class SymmetricTree {
+
+    public static void main(String[] strings) {
+        TreeNode root = TreeNodeCreator.createTreeNode("1, 2, 2, 3, 5, 4, 3");
+        SymmetricTree symmetricTree = new SymmetricTree();
+        System.out.println(symmetricTree.isSymmetric(root));
+    }
+
     public boolean isSymmetric(TreeNode root) {
         if (root == null) return true;
         else return areSymSubtrees(root.left, root.right);
     }
 
-    public boolean areSymSubtrees(TreeNode node1, TreeNode node2) {
-        if (node1 == null || node2 == null) return ((node1 == null) && (node2 == null));
-        if (node1.val != node2.val) return false;
-        boolean outsideSym = areSymSubtrees(node1.left, node2.right);
-        boolean insideSym = areSymSubtrees(node1.right, node2.left);
-        return outsideSym && insideSym;
+    public boolean areSymSubtrees(TreeNode left, TreeNode right) {
+        if (left == null || right == null) return ((left == null) && (right == null));
+        return left.val == right.val && areSymSubtrees(left.left, right.right) && areSymSubtrees(left.right, right.left);
     }
 }
