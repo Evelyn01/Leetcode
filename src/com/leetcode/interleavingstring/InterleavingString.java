fc4f61f24e3ca16ec596603bd6d777c4a82dbddb
duanbo1983 <duanbo1983@gmail.com>
2014-12-08 14:28:43 -0800

    Formatting.

--- src/com/leetcode/interleavingstring/InterleavingString.java
+++ src/com/leetcode/interleavingstring/InterleavingString.java
@@ -15,27 +15,23 @@ public class InterleavingString {
     }
 
     public boolean isInterleave(String s1, String s2, String s3) {
-        if (s3.length() != s1.length() + s2.length())
-        {
+        if (s3.length() != s1.length() + s2.length()) {
             return false;
         }
         int n = s1.length();
         int m = s2.length();
-        boolean[][] dp = new boolean[n+1][m+1];
-        for (boolean[] row : dp)
-        {
+        boolean[][] dp = new boolean[n + 1][m + 1];
+        for (boolean[] row : dp) {
             Arrays.fill(row, false);
         }
 
         dp[0][0] = true;
-        for (int i=0; i<=n; ++i)
-        {
-            for (int j=0; j<=m; ++j)
-            {
+        for (int i = 0; i <= n; ++i) {
+            for (int j = 0; j <= m; ++j) {
                 if (i > 0)
-                    dp[i][j] = (dp[i-1][j] && s1.charAt(i-1) == s3.charAt(i+j-1));
+                    dp[i][j] = (dp[i - 1][j] && s1.charAt(i - 1) == s3.charAt(i + j - 1));
                 if (j > 0)
-                    dp[i][j] = dp[i][j] || (dp[i][j-1] && s2.charAt(j-1) == s3.charAt(i+j-1));
+                    dp[i][j] = dp[i][j] || (dp[i][j - 1] && s2.charAt(j - 1) == s3.charAt(i + j - 1));
             }
         }
 
