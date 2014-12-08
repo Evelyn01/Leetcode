1f22fa9c76c791868f56e3db538dd18e3d641401
duanbo1983 <duanbo1983@gmail.com>
2014-12-08 15:09:46 -0800

    Add iterative solution.

--- src/com/leetcode/symmetrictree/SymmetricTree.java
+++ src/com/leetcode/symmetrictree/SymmetricTree.java
@@ -3,6 +3,8 @@ package com.leetcode.symmetrictree;
 import com.leetcode.common.TreeNode;
 import com.leetcode.common.TreeNodeCreator;
 
+import java.util.Stack;
+
 /**
  * Created by bod on 9/7/14.
  * https://oj.leetcode.com/problems/symmetric-tree/
@@ -23,5 +25,27 @@ public class SymmetricTree {
     public boolean areSymSubtrees(TreeNode left, TreeNode right) {
         if (left == null || right == null) return ((left == null) && (right == null));
         return left.val == right.val && areSymSubtrees(left.left, right.right) && areSymSubtrees(left.right, right.left);
+    }
+
+    public boolean isSymmetricIterative(TreeNode root) {
+        // IMPORTANT: Please reset any member data you declared, as
+        // the same Solution instance will be reused for each test case.
+        if (root == null) return true;
+        Stack<TreeNode> s1 = new Stack<TreeNode>();
+        Stack<TreeNode> s2 = new Stack<TreeNode>();
+        s1.push(root.left);
+        s2.push(root.right);
+        while (!s1.empty() && !s2.empty()) {
+            TreeNode n1 = s1.pop();
+            TreeNode n2 = s2.pop();
+            if (n1 == null && n2 == null) continue;
+            if (n1 == null || n2 == null) return false;
+            if (n1.val != n2.val) return false;
+            s1.push(n1.left);
+            s2.push(n2.right);
+            s1.push(n1.right);
+            s2.push(n2.left);
+        }
+        return true;
     }
 }
