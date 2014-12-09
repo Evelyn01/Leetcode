52e93ed284fe5a0c53afe16aea9ef21c233e231a
duanbo1983 <duanbo1983@gmail.com>
2014-12-09 10:15:20 -0800

    Add practice.

--- src/com/leetcode/pascaltriangle2/PascalTriangle2.java
+++ src/com/leetcode/pascaltriangle2/PascalTriangle2.java
@@ -19,7 +19,7 @@ public class PascalTriangle2 {
 
         System.out.println();
 
-        list = triangle2.getRowByMath(4);
+        list = triangle2.getRowByMath(6);
 
         for (int v : list) {
             System.out.print(v + ", ");
@@ -40,26 +40,39 @@ public class PascalTriangle2 {
         return result;
     }
 
+    /**
+     *
+     *              1
+     *            1   1
+     *          1   2   1
+     *        1   3   3   1
+     *      1   4   6   4   1
+     *    1   5  10   10  5   1
+     *  1   6  15   20  15  6   1
+     *
+     *
+     */
+
     public List<Integer> getRowByMath(int rowIndex) {
         List<Integer> resultList = new ArrayList<Integer>();
-        if (rowIndex==0){
+        if (rowIndex == 0) {
             resultList.add(1);
             return resultList;
         }
 
-        int num = rowIndex/2;
+        int num = rowIndex / 2;
         long temp = 1; // Some test cases have numbers larger than Integer.MAX_VALUE
-        resultList.add((int)temp);
+        resultList.add((int) temp);
 
-        // Compute first half using C(n, m) = C(n, m-1) * (n-m+1) / m;
-        for(int i = 1; i<=num; i++){
-            temp*=rowIndex-i+1;
-            temp/=i;
-            resultList.add((int)temp);
+        // Compute first half using C(n, m) = C(n, m - 1) * (n - m + 1) / m;
+        for (int i = 1; i <= num; i++) {
+            temp *= rowIndex - i + 1;
+            temp /= i;
+            resultList.add((int) temp);
         }
 
         // Mirror the second half
-        for(int i = (rowIndex-1)/2; i>=0; i--){
+        for (int i = (rowIndex - 1) / 2; i >= 0; i--) {
             resultList.add(resultList.get(i));
         }
 
