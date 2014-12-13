22fb6a86fe36c035b2f6e30dffa9a8c5fba9c09c
duanbo1983 <duanbo1983@gmail.com>
2014-12-13 09:44:58 -0800

    Add a solution.

--- src/com/linkcode/MaxSubarrayIII.java
+++ src/com/linkcode/MaxSubarrayIII.java
@@ -4,23 +4,28 @@ import java.util.*;
 
 /**
  * Created by titan-developer on 12/11/14.
+ * http://lintcode.com/en/problem/maximum-subarray-iii
  */
 public class MaxSubarrayIII {
 
     public static void main(String[] strings) {
         int[] a = {-1, 4, -2, 3, -2, 3};
         MaxSubarrayIII maxSubarrayIII = new MaxSubarrayIII();
-        System.out.println(maxSubarrayIII.maxKSubArrays(a, 2));
+
+        ArrayList<Integer> nums = new ArrayList<Integer>();
+        for (int v : a) nums.add(v);
+
+        System.out.println(maxSubarrayIII.maxSubArray(nums, 3));
     }
 
-    public int maxKSubArrays(int[] nums, int k) {
-        int[][] maxs = new int[nums.length][nums.length];
+    public int maxSubArray(ArrayList<Integer> nums, int k) {
+        int[][] maxs = new int[nums.size()][nums.size()];
 
-        for (int i = 0; i < nums.length; ++i) {
-            int max_till = nums[i];
-            maxs[i][i] = nums[i];
-            for (int j = i + 1; j < nums.length; ++j) {
-                max_till = Math.max(nums[j], max_till + nums[j]);
+        for (int i = 0; i < nums.size(); ++i) {
+            int max_till = nums.get(i);
+            maxs[i][i] = max_till;
+            for (int j = i + 1; j < nums.size(); ++j) {
+                max_till = Math.max(nums.get(j), max_till + nums.get(j));
                 maxs[i][j] = Math.max(maxs[i][j - 1], max_till);
             }
         }
@@ -28,7 +33,7 @@ public class MaxSubarrayIII {
         int[] dp = maxs[0];
         for (int m = 1; m < k; ++m) {
             int[] pre = new int[dp.length];
-            for (int i = m; i < nums.length; ++i) {
+            for (int i = m; i < nums.size(); ++i) {
                 int max_i = Integer.MIN_VALUE;
                 for (int j = m - 1; j < i; ++j) {
                     max_i = Math.max(max_i, dp[j] + maxs[j + 1][i]);
