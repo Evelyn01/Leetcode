9f06675f151689d6cb3a79ffcd42ffa0dcc8e419
duanbo1983 <duanbo1983@gmail.com>
2014-12-20 21:40:24 -0800

    Add common util.

---
+++ src/com/leetcode/common/ListPrinter.java
@@ -0,0 +1,17 @@
+package com.leetcode.common;
+
+import java.util.List;
+
+/**
+ * Created by titan-developer on 12/20/14.
+ */
+public class ListPrinter <E> {
+    public void output(List<List<E>> lists) {
+        for (List<E> list : lists) {
+            for (E e : list) {
+                System.out.print(e + ", ");
+            }
+            System.out.println();
+        }
+    }
+}
