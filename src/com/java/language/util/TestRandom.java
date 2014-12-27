3542f635f4a05d032a51af932ebb7197a4071bd0
duanbo1983 <duanbo1983@gmail.com>
2014-12-27 14:54:55 -0800

    Add a test.

---
+++ src/com/java/language/util/TestRandom.java
@@ -0,0 +1,18 @@
+package com.java.language.util;
+
+import java.util.Random;
+
+/**
+ * Created by titan-developer on 12/27/14.
+ */
+public class TestRandom {
+    public static void main(String[] strings) {
+        Random r1 = new Random(101);
+        Random r2 = new Random(101);
+        for (int i = 0; i < 10; i++) {
+            System.out.println(r1.nextInt());
+            System.out.println(r2.nextInt());
+            System.out.println("---");
+        }
+    }
+}
