17305eb7ce9f5f9cbfb0cd113bf266cd8c740894
Bo <titanduan1083@gmail.com>
2015-01-05 14:37:30 -0800

    Add a generic test.

--- src/com/java/language/util/generic/WildcardError.java
+++ src/com/java/language/util/generic/WildcardError.java
@@ -9,5 +9,12 @@ public class WildcardError {
 
     void foo(List<?> i) {
         //i.set(0, i.get(0)); //compile error
+        fooHelper(i);
+    }
+
+    // Helper method created so that the wildcard can be captured
+    // through type inference.
+    private <T> void fooHelper(List<T> l) {
+        l.set(0, l.get(0));
     }
 }
