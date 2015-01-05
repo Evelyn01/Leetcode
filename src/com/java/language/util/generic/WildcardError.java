5a4d40cac815e9e84b7555be7eb4dd6119fbd52b
Bo <titanduan1083@gmail.com>
2015-01-05 12:55:45 -0800

    Add a generic test.

---
+++ src/com/java/language/util/generic/WildcardError.java
@@ -0,0 +1,13 @@
+package com.java.language.util.generic;
+
+import java.util.List;
+
+/**
+ * Created by bod on 1/5/2015.
+ */
+public class WildcardError {
+
+    void foo(List<?> i) {
+        //i.set(0, i.get(0)); //compile error
+    }
+}
