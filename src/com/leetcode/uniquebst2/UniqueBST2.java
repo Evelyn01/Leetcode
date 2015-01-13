24212fd20c113d8734becef8dc82545f9b668587
duanbo1983 <duanbo1983@gmail.com>
2015-01-13 08:33:28 -0800

    Renaming.

--- src/com/leetcode/uniquebst2/UniqueBST2.java
+++ src/com/leetcode/uniquebst2/UniqueBST2.java
@@ -51,4 +51,63 @@ public class UniqueBST2 {
         node.right = clone(n.right, offset);
         return node;
     }
+
+    //-------------------------------------------------------------
+
+    public List<TreeNode> generateTreesRecursive(int n) {
+        List<TreeNode> ret;
+        if (n == 0) {
+            ret = new ArrayList<TreeNode>();
+            ret.add(null);
+        } else {
+            ret = helper(1, n);
+        }
+        return ret;
+    }
+
+    List<TreeNode> helper(int from, int to) {
+        List<TreeNode> ret = new ArrayList<TreeNode>();
+
+        if (from == to) {
+            TreeNode root = new TreeNode(from);
+            ret.add(root);
+            return ret;
+        }
+
+        for (int left = 0; left < (to - from + 1); left ++) {
+            int rootVal = from + left;
+            List<TreeNode> leftList = null, rightList = null;
+            int right = to - from - left;
+            if (left > 0) {
+                leftList = helper(from, from + left - 1);
+            }
+            if (right > 0) {
+                rightList = helper(from + left + 1, to);
+            }
+            if (leftList == null) {
+                leftList = new ArrayList<TreeNode>();
+                leftList.add(null);
+            }
+            if (rightList == null) {
+                rightList = new ArrayList<TreeNode>();
+                rightList.add(null);
+            }
+            collect(leftList, rootVal, rightList, ret);
+        }
+
+        return ret;
+    }
+
+    void collect(List<TreeNode> leftList, int rootVal, List<TreeNode> rightList, List<TreeNode> ret) {
+        for (int i = 0; i < leftList.size(); i ++) {
+            for (int j = 0; j < rightList.size(); j ++) {
+                TreeNode root = new TreeNode(rootVal);
+                TreeNode leftNode = leftList.get(i);
+                TreeNode rightNode = rightList.get(j);
+                root.left = leftNode;
+                root.right = rightNode;
+                ret.add(root);
+            }
+        }
+    }
 }
--- src/com/practise1/TestPractise.java
+++ src/com/practise1/TestPractise.java
@@ -1,6 +1,8 @@
 package com.practise1;
 
 import com.leetcode.util.ListNode;
+import com.leetcode.util.TreeNode;
+import com.leetcode.util.TreeNodePrinter;
 
 import java.math.BigInteger;
 import java.util.*;
@@ -24,8 +26,15 @@ public class TestPractise {
         };
 
         TestPractise testPractise = new TestPractise();
-        outputList(testPractise.grayCode(3));
+        outputTree(testPractise.generateTrees(0));
         System.out.println();
+    }
+
+    private static void outputTree(List<TreeNode> treeNodes) {
+        for (TreeNode root : treeNodes) {
+            TreeNodePrinter.printNode(root);
+            System.out.println("------------------");
+        }
     }
 
     public static <E> void outputList(List<E> list) {
@@ -35,27 +44,61 @@ public class TestPractise {
         System.out.println();
     }
 
-    public List<Integer> grayCode(int n) {
-        List<Integer> ret = new ArrayList<Integer>();
-        if (n <= 0) return ret;
-        ret.add(0);
-        boolean[] set = new boolean[1 << n];
-        set[0] = true;
-        int count = 1, current = 0;
-        while (count < set.length) {
-            for (int i = 0; i < n; i ++) {
-                int mask = 1 << i;
-                int v = current ^ mask;
-                if (!set[v]) {
-                    set[v] = true;
-                    count ++;
-                    current = v;
-                    ret.add(v);
-                    break;
-                }
-            }
+    public List<TreeNode> generateTrees(int n) {
+        List<TreeNode> ret;
+        if (n == 0) {
+            ret = new ArrayList<TreeNode>();
+            ret.add(null);
+        } else {
+            ret = helper(1, n);
         }
         return ret;
+    }
+
+    List<TreeNode> helper(int from, int to) {
+        List<TreeNode> ret = new ArrayList<TreeNode>();
+
+        if (from == to) {
+            TreeNode root = new TreeNode(from);
+            ret.add(root);
+            return ret;
+        }
+
+        for (int left = 0; left < (to - from + 1); left ++) {
+            int rootVal = from + left;
+            List<TreeNode> leftList = null, rightList = null;
+            int right = to - from - left;
+            if (left > 0) {
+                leftList = helper(from, from + left - 1);
+            }
+            if (right > 0) {
+                rightList = helper(from + left + 1, to);
+            }
+            if (leftList == null) {
+                leftList = new ArrayList<TreeNode>();
+                leftList.add(null);
+            }
+            if (rightList == null) {
+                rightList = new ArrayList<TreeNode>();
+                rightList.add(null);
+            }
+            collect(leftList, rootVal, rightList, ret);
+        }
+
+        return ret;
+    }
+
+    void collect(List<TreeNode> leftList, int rootVal, List<TreeNode> rightList, List<TreeNode> ret) {
+        for (int i = 0; i < leftList.size(); i ++) {
+            for (int j = 0; j < rightList.size(); j ++) {
+                TreeNode root = new TreeNode(rootVal);
+                TreeNode leftNode = leftList.get(i);
+                TreeNode rightNode = rightList.get(j);
+                root.left = leftNode;
+                root.right = rightNode;
+                ret.add(root);
+            }
+        }
     }
 }
 
